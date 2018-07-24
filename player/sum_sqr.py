d904058b125a3480a52c169f987d30dfaccd0b7d
HassanAcet <syedhassan461@gmail.com>
2018-07-24 11:27:13 +0530

    Update sum_sqr.py

--- player/sum_sqr.py
+++ player/sum_sqr.py
@@ -1,4 +1,4 @@
-def sumdigsqr(n):
+def sumdsqr(n):
 	sum=0
 	while(n!=0):
 		r=n%10
@@ -7,7 +7,7 @@ def sumdigsqr(n):
 	return sum
 def happy(n):
 	while(n>0):
-		n=sumdigsqr(n)
+		n=sumdsqr(n)
 		if n==1:
 			print('happy number')
 			return
